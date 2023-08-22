dde7638b984e803301c7a7f4186d9152e9aeeb72
Lyjia <me@lyjia.us>
2023-08-21 22:46:07 -0700

    add readme, flatLAF, and make startup a window

---
+++ README.md
@@ -0,0 +1,10 @@
+# NiceYtDlpGui
+
+A graphical frontend for yt_dlp by [Lyjia](http://www.lyjia.us)
+
+Built with GraalVM 20
+
+To build native executable run `mvn -P native package` (currently broken)
+
+Otherwise just use `mvn package`
+
--- src/main/java/us/lyjia/Main.java
+++ src/main/java/us/lyjia/Main.java
@@ -1,7 +1,28 @@
 package us.lyjia;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import java.io.*;
+import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    try {
+        UIManager.setLookAndFeel( new FlatLightLaf() );
+    } catch( Exception ex ) {
+        System.err.println( "Failed to initialize LaF" );
+    }
+    
+    JFrame frame = new JFrame();
+    
+    JButton button = new JButton("Hello world!");
+    
+    button.setBounds(150, 200, 220, 50);
+    
+    frame.add(button);
+    
+    frame.setSize(500, 600);
+    frame.setLayout(null);
+    frame.setVisible(true);
   }
 }
