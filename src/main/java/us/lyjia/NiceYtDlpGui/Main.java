32a961408ab55c6d7f25bf0ce33f01af6d369929
Lyjia <me@lyjia.us>
2023-08-25 14:52:41 -0700

    override java.home to working dir if it is detected to be empty

--- src/main/java/us/lyjia/NiceYtDlpGui/Main.java
+++ src/main/java/us/lyjia/NiceYtDlpGui/Main.java
@@ -4,9 +4,17 @@ import com.formdev.flatlaf.FlatLightLaf;
 import us.lyjia.NiceYtDlpGui.windows.MainWindow;
 
 import javax.swing.*;
+import java.io.File;
 
 public class Main {
   public static void main(String[] args) {
+    
+    // Possible fix for
+    // https://github.com/oracle/graal/issues/3659
+    if (System.getProperty("java.home") == null) {
+        System.out.println("No java.home is set, seems this is a native-image from GraalVM. Fixing...");
+        System.setProperty("java.home", new File(".").getAbsolutePath());
+    }
     
     try {
         UIManager.setLookAndFeel( new FlatLightLaf() );
